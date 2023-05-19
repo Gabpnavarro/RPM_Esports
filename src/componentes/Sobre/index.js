fe5875d4a0366315be4aca11157da681c890d43c
Gabriel Navarro <118223745+Gabpnavarro@users.noreply.github.com>
2023-05-19 14:33:34 -0300

    Update index.js

--- src/componentes/Sobre/index.js
+++ src/componentes/Sobre/index.js
@@ -14,7 +14,7 @@ function Sobre() {
             <div className={styles.sobre__conhecaOGrid}>
                 <h2>CONHEÇA O GRID</h2>
                 <p>
-                    Seja bem vindo ao novo formato de grids na RPM Esports!agora com 2 grids no mesmo dia! 20:00 (LIGHT) 22:15 (PRO)
+                    Seja bem vindo ao novo formato de grids na RPM Esports! Agora com 2 grids no mesmo dia! 20:00 (LIGHT) 22:15 (PRO)
                     com promoção e rebaixamento da forma mais justa! o MOD utilizado é o STOCK CAR no Assetto Corsa 1.
                 </p>
                 <h2>CONHEÇA A CLASSE & CARROS</h2>
